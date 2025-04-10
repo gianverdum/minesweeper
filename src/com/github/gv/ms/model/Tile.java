fc9873378104f020aac8c79488bbcd4296509ebe
gianverdum <gian.verdum@gmail.com>
2025-04-10 12:04:48 -0300

    Initial Tile definition restored

--- src/com/github/gv/ms/model/Tile.java
+++ src/com/github/gv/ms/model/Tile.java
@@ -1,4 +1,25 @@
 package com.github.gv.ms.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tile {
+
+    private final int row;
+    private final int col;
+
+    private boolean isOpen;
+    private boolean hasMine;
+    private boolean hasFlag;
+
+    private List<Tile> neighbors = new ArrayList<Tile>();
+
+    Tile(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    boolean addNeighbor(Tile neighbor) {
+        return neighbors.add(neighbor);
+    }
 }
